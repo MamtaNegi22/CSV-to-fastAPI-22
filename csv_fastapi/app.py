fa25b7b22f5fde3639b0ff331911681923f6af7b
MamtaNegi22 <mamtanegi440@gmail.com>
2026-03-25 23:50:56 +0530

    data set loaded

--- csv_fastapi/app.py
+++ csv_fastapi/app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app=FastAPI()
+
+# -----------data_load------------------
+
+def load_data():
+    with open("students_complete.csv","r") as f:
+        data=pd.read_csv(f)
+    return data
 
 @app.get("/")
 def home():
@@ -9,3 +17,10 @@ def home():
 @app.get("/about")
 def about():
     return "here you will get the data from the csv file"
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data.to_json(orient="records")
+# to_dict se nhi hoga - Pandas converts structure → but NOT always datatypes
+# to_json will work - Pandas converts everything into a JSON string
